add1900a3416b6a124c79e9a2592a75b6848f5a6
frostedoyster <bigi.f@libero.it>
2026-01-07 13:00:12 +0100

    Eliminate useless files

--- models/pet/check.py
+++
@@ -1,7 +0,0 @@
-from metatomic.torch.ase_calculator import MetatomicCalculator
-from metatomic.torch.ase_calculator import SymmetrizedCalculator
-
-
-calc = SymmetrizedCalculator(MetatomicCalculator("pet-oam-1epoch-55.pt", device="cuda"))
-for name, param in calc._model.named_parameters():
-    print(name, param.shape)
